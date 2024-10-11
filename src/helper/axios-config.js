00bc0cfc72b319f383510e6754a33b0473bb3512
Santiago <sastijim2010@gmail.com>
2024-10-11 17:37:42 -0500

    Render

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000'
+    // baseURL: 'http://localhost:4000'
+    baseURL: 'https://peliculas-backend-lfz5.onrender.com/'
 });
 
 export{
